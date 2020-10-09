e6e081281432b5b051a7452ba57e881b6972018c
Josh Belmont <jbelmont19@live.com>
2020-10-09 08:54:04 -0700

    Initial Commit

---
+++ BrainGameChallenge/BrainGameChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainGameChallengeApp.swift
+//  BrainGameChallenge
+//
+//  Created by Josh Belmont on 10/9/20.
+//
+
+import SwiftUI
+
+@main
+struct BrainGameChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrainGameChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BrainGameChallenge
+//
+//  Created by Josh Belmont on 10/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
